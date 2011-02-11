08af63147d14c0151f18921bc037902a1197aa73
Avalos <avaloscesar@gmail.com>
2011-02-11 14:50:45 -0200

    Updating acl syntax and README

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 # Require awk
 
-
-define acl_udefault( $id, $mode, $path) {
+define udefault( $id, $mode, $path) {
         exec { "apply_udefault":
                 command   => "/usr/bin/setfacl -m d:u:$id:$mode $path",
                 onlyif => "/usr/bin/getfacl $path  2>&1 | awk -F: ' \$1 ~ /^default/ && \$2 ~ /user/ && \$3 ~/$id/  && \$4 ~ /$mode/  { exit 1 } '  "
@@ -9,7 +8,7 @@ define acl_udefault( $id, $mode, $path) {
 }
 
 
-define acl_gdefault( $id, $mode, $path) {
+define gdefault( $id, $mode, $path) {
         exec { "apply_gdefault":
                 command   => "/usr/bin/setfacl -m d:g:$id:$mode $path",
                 onlyif => "/usr/bin/getfacl $path  2>&1 | awk -F: ' \$1 ~ /^default/ && \$2 ~ /group/ && \$3 ~/$id/  && \$4 ~ /$mode/  { exit 1 } '  "
@@ -17,7 +16,7 @@ define acl_gdefault( $id, $mode, $path) {
 }
 
 
-define acl_group( $id, $mode, $path) {
+define group( $id, $mode, $path) {
         exec { "apply_group":
                 command   => "/usr/bin/setfacl -m g:$id:$mode $path",
                 onlyif => "/usr/bin/getfacl $path  2>&1 | awk -F: ' \$1 ~ /^group/ && \$2 ~/$id/  && \$3 ~ /$mode/  { exit 1 } '  "
@@ -25,28 +24,10 @@ define acl_group( $id, $mode, $path) {
 }
  
 
-define acl_user( $id, $mode, $path) {
+define user( $id, $mode, $path) {
         exec { "apply_user":
                 command   => "/usr/bin/setfacl -m u:$id:$mode $path",
                 onlyif => "/usr/bin/getfacl $path 2>&1 | awk -F: ' \$1 ~ /^user/ && \$2 ~/$id/  && \$3 ~ /$mode/  { exit 1 } '  "
         }
 }
-
-# Acceptable modes :
-
-# rwx
-# rw-
-# r--
-# --x
-
-# I think you got it right ?
-
-
-
-# Example of usage
-# acl_user { "avalos_rw_rc.local" :
-# id => avalos,
-# mode => "rw-",
-# path => "/etc/rc.local",
-# }
 
